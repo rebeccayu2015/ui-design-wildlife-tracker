f59f0331d26feaa3e3a2966547c735ba2dcf2478
Brianna Wang <wang.brianna@gmail.com>
2025-05-06 16:40:16 -0400

    implemented drag and drop on sort prints

--- static/js/quiz_sort_prints.js
+++ static/js/quiz_sort_prints.js
@@ -3,4 +3,17 @@ $(document).ready(function () {
         $(".next-btn").removeClass('d-none')
         $(".next-btn").addClass('d-block')
     })
+
+    $(".suspect-track").draggable({
+        revert: true,
+        cursor: "move",
+        zIndex: 1000,
+    });
+
+    $(".family-box").droppable({
+        //accept: ".suspect-track",
+        drop: function (event, ui) {
+            $(this).append(ui.draggable);
+        }
+    });
 });
