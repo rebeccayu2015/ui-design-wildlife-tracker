ae2ad22203ceb6e9bf1e680fe3c34ed543842cb7
Brianna Wang <wang.brianna@gmail.com>
2025-05-06 17:35:05 -0400

    implemented feedback and scoring for sort prints

--- static/js/quiz_sort_prints.js
+++ static/js/quiz_sort_prints.js
@@ -1,7 +1,47 @@
 $(document).ready(function () {
     $(".sort-btn").on("click", function () {
-        $(".next-btn").removeClass('d-none')
-        $(".next-btn").addClass('d-block')
+    
+        $(".family-box").each(function () {
+            let familyId = $(this).attr("id");
+            let family = familyId.split("-")[1];
+            let suspects = [];
+    
+            $(this).find(".suspect-track").each(function () {
+                let suspectId = $(this).attr("id");
+                suspects.push(suspectId.split("-")[1]);
+            });
+    
+            sort_prints_responses[family] = suspects;
+        });
+        console.log(sort_prints_responses);
+
+        // send responses to server
+        $.ajax({
+            type: "POST",
+            url: "/check_sort_prints",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(sort_prints_responses),
+            success: function (response) {
+                let correct_matches = response['correct_matches']   
+                let total_number = response['total_number']
+                if (correct_matches == total_number) { // TODO: add condition to make sure that total_number = total number of prints
+                    $("#sort-prints-feedback").text("Great job! You matched all " + correct_matches + "/" + total_number + " prints correctly. Click next to continue.");
+                    $(".next-btn").removeClass('d-none')
+                    $(".next-btn").addClass('d-block')
+                }
+                else{ // TODO: add condition for case where not all the prints are sorted but all the sorted ones are correct
+                    $("#sort-prints-feedback").text("You matched " + correct_matches + "/" + total_number + " prints correctly. Try again!");
+                }
+                console.log("Success");
+            },
+            error: function (request, status, error) {
+                console.log("Error");
+                console.log(request);
+                console.log(status);
+                console.log(error);
+            }
+        });
     })
 
     $(".suspect-track").draggable({
